4ee4eeb99cbd4ee84eed66fea60e81ed48459fe4
dasdfadssda <quddkflty@gmail.com>
2024-01-12 22:17:16 +0900

    fix : refactoring

--- src/Components/Common/LogDiv_Comppnents.js
+++ src/Components/Common/LogDiv_Comppnents.js
@@ -3,13 +3,60 @@ import { auth } from "../../fbase";
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+/*
+- 화면 로딩시 토큰 조회
+  - 토큰 조회 실패시 로그인 화면으로 이동
+- 로그아웃 코드
+- Main 화면 코드
+*/
+
+const CommonLogSection = () => {
+  const navigate = useNavigate();
+
+  // 화면 로딩시 토큰 조회
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const userName = localStorage.getItem("userName");
+
+    // 토큰 조회 실패시 로그인 화면으로 이동
+    if (!token || !userName) {
+      alert("로그인이 필요합니다.");
+      navigate("/Login");
+    }
+  }, []);
+
+  // 로그아웃 코드
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      alert("로그아웃되었습니다.");
+      localStorage.removeItem("token");
+      localStorage.removeItem("userName");
+      navigate("/Login");
+      window.location.reload();
+    } catch (error) {
+      console.error("로그아웃 중 오류 발생:", error);
+    }
+  };
+
+  // Main 화면 코드
+  return (
+    <LogDiv>
+      <NameText>{localStorage.getItem("userName")}</NameText>
+      <BarText />
+      <LogText onClick={handleLogout}>로그아웃</LogText>
+    </LogDiv>
+  );
+};
+
+export default CommonLogSection;
+
 const LogDiv = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
   margin-top: 32px;
   margin-right: 64px;
-  /* background-color: red; */
 `;
 
 const NameText = styled.div`
@@ -39,39 +86,3 @@ const BarText = styled.div`
   margin-right: 14px;
   background: linear-gradient(92deg, #5262f5 0%, #7b3fef 100%);
 `;
-
-const CommonLogSection = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    const userName = localStorage.getItem("userName");
-
-    if (!token || !userName) {
-      alert("로그인이 필요합니다.");
-      navigate("/Login");
-    }
-  }, []);
-  const handleLogout = async () => {
-    try {
-      await auth.signOut();
-      alert("로그아웃되었습니다.");
-      localStorage.removeItem("token");
-      localStorage.removeItem("userName");
-      navigate("/Login");
-      window.location.reload();
-    } catch (error) {
-      console.error("로그아웃 중 오류 발생:", error);
-    }
-  };
-
-  return (
-    <LogDiv>
-      <NameText>{localStorage.getItem("userName")}</NameText>
-      <BarText />
-      <LogText onClick={handleLogout}>로그아웃</LogText>
-    </LogDiv>
-  );
-};
-
-export default CommonLogSection;
